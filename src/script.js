c2cd9c4328b84191c163f4b883ba3a9a06eb5bb7
ftmkhosravi <109632695+ftmkhosravi@users.noreply.github.com>
2023-03-28 05:34:51 +0430

    update the time

--- src/script.js
+++ src/script.js
@@ -1,17 +1,37 @@
 let apiKey = "4aa4f4c485e2f9ad87e3fd6f892979f5";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=${apiKey}&units=metric`;
 
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days =["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    if(minutes<10){
+        minutes = `0${minutes}`
+    }
+    if(hours<10){
+        hours = `0${hours}`
+    }
+    return `${day}  ${hours}:${minutes}`;
+}
+
+
+
 function displayTemperature(response){
     let temp = document.querySelector("#temperature");
-    temp.innerHTML = Math.round(response.data.main.temp);
     let city = document.querySelector("#city");
-    city.innerHTML = response.data.name;
     let description = document.querySelector("#description");
-    description.innerHTML = response.data.weather[0].description;
     let humidity = document.querySelector("#humidity");
-    humidity.innerHTML = Math.round(response.data.main.humidity);
     let wind = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    temp.innerHTML = Math.round(response.data.main.temp);
+    city.innerHTML = response.data.name;
+    description.innerHTML = response.data.weather[0].description;
+    humidity.innerHTML = Math.round(response.data.main.humidity);
     wind.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt *1000);
+    
     
 }
 axios.get(apiUrl).then(displayTemperature);
